138a1e4ca0725dc0eb646be6c219693e089dbcda
JCBoyd <jcboyd@pm.me>
2020-03-04 12:30:12 -0700

    Version 1

--- src/vendingmachineinventory/Inventory.java
+++ src/vendingmachineinventory/Inventory.java
@@ -7,7 +7,45 @@ package vendingmachineinventory;
  * This class is associated with VendingMachineInventory.java
  *****************************************************************************/
 
+import java.util.Scanner;
+
 public class Inventory 
 {
+    Scanner stdIn = new Scanner(System.in);
+    private String name;
+    private double price;
+    private int quantity;
+
+    //*************************************************************************
     
+    public Inventory(String name, double price, int quantity)
+    {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    } //end Inventory constructor
+                
+    //*************************************************************************
+            
+    public void display()
+    {
+        System.out.printf("Name: %s, price: $%.2f, Quantity: %d\n",
+                this.name, this.price, this.quantity);
+    } //end display
+    
+    //*************************************************************************
+    
+    public void newStock()
+    {
+        int quantity; //hold input value for quantity of new stock
+        double price; //hold total price of new stock
+        System.out.printf("How many bottles of %s are you delivering? ", this.name);
+        quantity = stdIn.nextInt();
+        stdIn.nextLine(); //clearing Scanner
+        this.quantity += quantity;
+        price = this.price * quantity;
+        System.out.printf("Received $%.2f of %s\n",
+                price, this.name);
+    } //end newStock
+            
 } //end class Inventory
--- src/vendingmachineinventory/VendingMachineInventory.java
+++ src/vendingmachineinventory/VendingMachineInventory.java
@@ -13,9 +13,26 @@ public class VendingMachineInventory {
 
     public static void main(String[] args) 
     {
-
-
-
+        //instantiate starting inventory
+        Inventory item1 = new Inventory("Mountain Dew", 1.5, 25);
+        Inventory item2 = new Inventory("water", 1, 15);
+        Inventory item3 = new Inventory("Apple juice", 2, 20);
+        //display current inventory
+        System.out.println("Module 3 Competency by JC Boyd");
+        System.out.println("\nStarting inventory:");
+        item1.display();
+        item2.display();
+        item3.display();
+        //accept delivery of additional inventory
+        System.out.println("");
+        item1.newStock();
+        item2.newStock();
+        item3.newStock();
+        //display updated (current) inventory
+        System.out.println("\nCurrent inventory:");
+        item1.display();
+        item2.display();
+        item3.display();
     } //end main
     
 } //end class VendingMachineInventory
