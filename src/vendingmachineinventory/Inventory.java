cbbf32e5ea817af045852913d27928bbb2bb8f9d
JCBoyd <jcboyd@pm.me>
2020-03-04 12:02:41 -0700

    Create shell, commit, and push to remote repository

---
+++ src/vendingmachineinventory/Inventory.java
@@ -0,0 +1,13 @@
+package vendingmachineinventory;
+
+/******************************************************************************
+ * Inventory.java 
+ * Programmer: @author jcboyd
+ * 
+ * This class is associated with VendingMachineInventory.java
+ *****************************************************************************/
+
+public class Inventory 
+{
+    
+} //end class Inventory
---
+++ src/vendingmachineinventory/VendingMachineInventory.java
@@ -0,0 +1,21 @@
+package vendingmachineinventory;
+
+/******************************************************************************
+ * VendingMachineInventory.java 
+ * Programmer: @author jcboyd
+ * Version: 1.0
+ * Course: SDEV 2210
+ *
+ * This program simulates the inventory and sales for a vending machine
+ *****************************************************************************/
+
+public class VendingMachineInventory {
+
+    public static void main(String[] args) 
+    {
+
+
+
+    } //end main
+    
+} //end class VendingMachineInventory
